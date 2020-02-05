4f407752d2d5bff893cabf78b48a3eb5ada35233
qwqfornoip <1914455815@qq.com>
2020-02-05 20:04:09 +0800

    Cookie test.

--- src/main/java/com/qwqnoi/community/controller/AuthorizeController.java
+++ src/main/java/com/qwqnoi/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -33,7 +35,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request, HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -42,17 +44,19 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUsr(accessToken);
-        //System.out.println("usr_name:" + githubUser.getName());
+
         if (githubUser != null){
             Usr usr = new Usr();
-            usr.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            usr.setToken(token);
             usr.setName(githubUser.getName());
             usr.setAccountId(String.valueOf(githubUser.getId()));
             usr.setGmtCreate(System.currentTimeMillis());
-            usr.setGmtModified(System.currentTimeMillis());
+            usr.setGmtModified(usr.getGmtCreate());
             usrMapper.insert(usr);
+
             //登录成功，写cookie和session
-            request.getSession().setAttribute("usr", githubUser);
+            response.addCookie(new Cookie("token", token));
             return "redirect:/";
         } else {
             //登录失败，从新登录
--- src/main/java/com/qwqnoi/community/mapper/UsrMapper.java
+++ src/main/java/com/qwqnoi/community/mapper/UsrMapper.java
@@ -3,6 +3,7 @@ package com.qwqnoi.community.mapper;
 import com.qwqnoi.community.model.Usr;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,7 @@ public interface UsrMapper {
     @Insert("INSERT INTO communityschema.usr (name, account_id, token, gmt_create, gmt_modified) " +
             "values (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModified})")
     public void insert(Usr usr);
+
+    @Select("SELECT * FROM communityschema.usr WHERE token=#{token}")
+    Usr findByToken(@Param("token") String token);
 }
